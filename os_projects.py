314933e329e2c66f56147e8dd1feaf2329367ffd
Jake Dupuy <jake.dupuy@gmail.com>
2017-03-23 20:38:47 -0500

    new module version

--- os_projects.py
+++ os_projects.py
@@ -74,108 +74,128 @@ EXAMPLES = '''
 
 try:
     from keystoneclient.v2_0 import client as ks_client
+    import inspect
+    import logging
     HAS_CLIENTS = True
 except ImportError:
     HAS_CLIENTS = False
 
+LOG = logging.getLogger(__name__)
+handler = logging.FileHandler('/var/log/chaperone/chaperone_project_create.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+LOG.addHandler(handler)
+LOG.setLevel(logging.DEBUG)
 
-os = {}
-
-
-def state_exit_unchanged(module):
-    project = None
-
-    ks = keystone_auth(module)
-
-    tenants = [t for t in ks.tenants.list()]
-
-    for tenant in tenants:
-        if tenant.name == module.params['new_project_name']:
-            project = tenant
-
-    module.exit_json(changed=False,
-                     msg="EXIT UNCHANGED",
-                     project_name=project.name,
-                     project_id=project.id)
+def log(message=None):
+    func = inspect.currentframe().f_back.f_code
+    msg="Method: {} Line Number: {} Message: {}".format(func.co_name, func.co_firstlineno, message)
+    LOG.debug(msg)
 
 
-def state_create_project(module):
+class OpenstackProject(object):
 
-    new_project, changed = create_project(module)
+    def __init__(self, module):
+        super(OpenstackProject, self).__init__()
+        self.module = module
+        self.auth_url = module.params['auth_url']
+        self.user_name = module.params['username']
+        self.user_pass = module.params['password']
+        self.auth_tenant = module.params['tenant_name']
+        self.project_name = module.params['new_project_name']
+        self.current_state = None
+        self.ks = self.keystone_auth()
 
-    if changed:
-        module.exit_json(changed=changed,
-                         project_name=new_project.name,
-                         project_id=new_project.id)
-    else:
-        module.exit_json(changed=changed, msg="failed to create project")
+    def keystone_auth(self):
+        log("GETTING KEYSTONE CLIENT....")
+        ksclient = None
+        try:
+            ksclient = ks_client.Client(username=self.user_name, password=self.user_pass,
+                                        tenant_name=self.auth_tenant, auth_url=self.auth_url,
+                                        insecure=True)
+        except Exception as e:
+            msg="Failed to get keystone client: {}".format(e)
+            log(msg)
+            self.module.fail_json(msg=msg)
+        return ksclient
 
+    def check_project_state(self):
+        log("Checking State...")
+        state = 'absent'
+        projects = [p.name for p in self.ks.tenants.list()]
+        if self.project_name in projects:
+            state = 'present'
+        log("Current State--> {}".format(state))
+        return state
 
+    def process_state(self):
+        log("Processing State...")
+        self.current_state = self.check_project_state()
 
-def state_update_project(module):
-    module.exit_json(changed=False, msg="Update Project")
+        project_states = {
+            'absent': {
+                'present': self.state_delete_project,
+                'absent': self.state_exit_unchanged,
+            },
+            'present': {
+                'present': self.state_exit_unchanged,
+                'absent': self.state_create_project,
+            }
+        }
 
-
-def state_delete_project(module):
-    module.exit_json(changed=False, msg="delete Project")
-
-
-def keystone_auth(module):
-
-    ksclient = None
-
-    try:
-        ksclient = ks_client.Client(username=module.params['username'],
-                                    password=module.params['password'],
-                                    tenant_name=module.params['tenant_name'],
-                                    auth_url=module.params['auth_url'],
-                                    insecure=True)
-    except Exception as e:
-        module.fail_json(msg="Failed to get keystone client authentication: {}".format(e))
-
-    return ksclient
-
-
-def create_project(module):
-    tenant = None
-    changed = False
-
-    ks = keystone_auth(module)
-
-    try:
-        tenant = ks.tenants.create(module.params['new_project_name'])
-        changed = True
-    except Exception as e:
-        module.fail_json(msg="Failed to create project: {}".format(e))
-
-    return tenant, changed
-
-
-def check_project_present(module):
-
-    ks = keystone_auth(module)
-
-    tenants = [t.name for t in ks.tenants.list()]
-
-    if module.params['new_project_name'] in tenants:
-        return True
-
-    return False
+        project_states[self.module.params['state']][self.current_state]()
 
 
-def check_project_state(module):
+    def state_create_project(self):
+        log("Create Project...")
+        new_project = None
+        changed = False
+        try:
+            new_project = self.ks.tenants.create(self.project_name)
+            changed = True
+        except Exception as e:
+            msg="Failed to create Project Exception: {}".format(e)
+            log(msg)
+            self.module.fail_json(msg=msg)
+        if not new_project:
+            self.module.fail_json(msg="Failed to create Project")
+        log("NEW PROJECT--> {}".format(new_project))
+        self.module.exit_json(changed=changed, project_name=new_project.name, project_id=new_project.id)
 
-    if check_project_present(module):
-        return 'present'
-    else:
-        return 'absent'
+    def state_delete_project(self):
+        log("Deleting Project...")
+        delete_project = None
+        project_name = None
+        project_id = None
+
+        project_to_delete = [t for t in self.ks.tenants.list() if t.name == self.project_name]
+
+        if project_to_delete:
+            project_name = project_to_delete[0].name
+            project_id = project_to_delete[0].id
+            delete_project = self.ks.tenants.delete(project_to_delete[0])
+        log("Deleted Project--> {}".format(project_name))
+        self.module.exit_json(changed=True, project_name=project_name,
+                              project_id=project_id, msg="Delete Project")
+
+    def state_exit_unchanged(self):
+        log("Exiting Unchanged...")
+        if self.module.params['state'] == 'absent' and self.current_state == 'absent':
+            self.module.exit_json(changed=False, msg="EXIT UNCHANGED",
+                                  project_name=None, project_id=None)
+
+        if self.module.params['state'] == 'present' and self.current_state == 'present':
+            project = [p for p in self.ks.tenants.list() if p.name == self.project_name][0]
+            self.module.exit_json(changed=False, msg="EXIT UNCHANGED",
+                                  project_name=project.name, project_id=project.id)
+
 
 
 def main():
     argument_spec = dict(
         auth_url=dict(required=True, type='str'),
         username=dict(required=True, type='str'),
-        password=dict(required=True, type='str'),
+        password=dict(required=True, type='str', no_log=True),
         tenant_name=dict(required=True, type='str'),
         new_project_name=dict(required=True, type='str'),
         state=dict(default='present', choices=['present', 'absent'], type='str'),
@@ -186,24 +206,8 @@ def main():
     if not HAS_CLIENTS:
         module.fail_json(msg='python-keystone is required for this module')
 
-    try:
-        project_states = {
-            'absent': {
-                'update': state_delete_project,
-                'present': state_delete_project,
-                'absent': state_exit_unchanged,
-            },
-            'present': {
-                'update': state_update_project,
-                'present': state_exit_unchanged,
-                'absent': state_create_project,
-            }
-        }
-
-        project_states[module.params['state']][check_project_state(module)](module)
-
-    except Exception as e:
-        module.fail_json(msg=str(e))
+    os = OpenstackProject(module)
+    os.process_state()
 
 
 from ansible.module_utils.basic import *
